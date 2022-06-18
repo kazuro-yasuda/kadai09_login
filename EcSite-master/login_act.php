08be90f1f7343f998c3cd90f6c57649a4f37caad
K.yasuda <kazuro_918@yahoo.co.jp>
2022-06-18 14:21:03 +0900

    UPDATE

--- EcSite-master/login_act.php
+++ EcSite-master/login_act.php
@@ -4,28 +4,39 @@ require("./dbconnect.php");
 // $db = db_conn();
 
 //POST値を受け取る
-$mail = $_POST['mail'];
-$pass = $_POST['pass'];
+ $password = $_POST['password'];
+
+
+//  echo $password;
+//
 
 //データ登録SQL作成
  //mailアドレスがあるか確認
-$stmt = $db->prepare("SELECT * FROM members WHERE mail = :mail");
-$stmt->bindValue(':mail', $mail, PDO::PARAM_STR);
+$stmt = $db->prepare("SELECT * FROM members WHERE password = :password");
+$stmt->bindValue(':password', $password, PDO::PARAM_STR);
 $status = $stmt->execute();
 
+// echo $stmt['password'];
+// exit();
 
 //抽出データ数を取得
 $member = $stmt->fetch();
+// exit();
 
-//指定したハッシュがパスワードにマッチしているかチェック
-if (password_verify($pass, $member['pass'])) {
+// echo $member['password'];
+// exit();
+
+//指定したハッシュがパスワードに一致しているかチェック
+if (password_verify($password, $member['password'])) {
     //DBのユーザー情報をセッションに保存
     $_SESSION['id'] = $member['id'];
     $_SESSION['name'] = $member['name'];
     $msg = 'ログインしました。';
+    // echo $msg;
     header('Location: index.php');
 } else {
     $msg = 'メールアドレスもしくはパスワードが間違っています。';
+    // echo $msg;
     $link = '<a href="login.php">戻る</a>';
 }
 ?>
